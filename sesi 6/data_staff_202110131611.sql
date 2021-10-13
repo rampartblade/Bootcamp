03ed8d08afceccdf4e08db26ddbf117c3abf0117
Rampartblade <rickyadiputra98@gmail.com>
2021-10-13 17:04:49 +0700

    initial commit

---
+++ sesi 6/data_staff_202110131611.sql
@@ -0,0 +1,7 @@
+INSERT INTO Staff.dbo.data_staff (nik,nama,alamat,handphone,joinDate) VALUES
+	 (1,N'Jimmy',N'Tangerang',N'081234567890',NULL),
+	 (2,N'Jimbo',N'Bogor',N'081234567890',NULL),
+	 (3,N'Amir',N'Bandung',N'9856714134',NULL),
+	 (4,N'Udin',N'Malang',N'3652147856',NULL),
+	 (5,N'Wumbo',N'Jakarta',N'548673219',NULL),
+	 (6,N'Joni',N'Yogyakarta',N'542384584','2021-10-13');
---
+++ sesi 6/staffoutsource_202110131611.sql
@@ -0,0 +1,11 @@
+INSERT INTO Staff.dbo.staffoutsource (nik,nama,alamat,handphone,joinDate) VALUES
+	 (1,N'Jimmy',N'Surabaya',N'08125469735','2021-10-13'),
+	 (2,N'Reyhan',N'Surabaya',N'08125469735','2021-03-05'),
+	 (3,N'Bayu',N'Semarang',N'5436887413','2021-03-05'),
+	 (4,N'Sinaga',N'Semarang',N'542641234','2021-03-06'),
+	 (5,N'Jimmy',N'Surabaya',N'542534123','2021-03-06'),
+	 (6,N'Jimmy',N'Surabaya',N'45721357','2021-10-05'),
+	 (7,N'Jimmy',N'Surabaya',N'457662366','2021-10-05'),
+	 (8,N'Jimmy',N'Surabaya',N'5681373','2021-10-13'),
+	 (9,N'Jimmy',N'Surabaya',N'66684678','2021-10-13'),
+	 (10,N'Jimmy',N'Surabaya',N'686255778','2021-10-13');
