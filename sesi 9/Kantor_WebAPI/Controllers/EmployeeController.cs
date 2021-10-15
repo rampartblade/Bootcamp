269b5b02eeb9fea15beb33eb74311a530fd101c0
Rampartblade <rickyadiputra98@gmail.com>
2021-10-15 13:03:02 +0700

    initial commit

---
+++ sesi 9/Kantor_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Kantor_WebAPI.Models;
+
+namespace Kantor_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeContext _context;
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        //GET: api/User
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            //return new string []
+            return _context.GetAllEmployee();
+        }
+
+        //GET: api/User/{id}
+        [HttpGet("{id}", Name ="Get")]
+
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems(String id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetEmployee(id);
+        }
+    }
+}
